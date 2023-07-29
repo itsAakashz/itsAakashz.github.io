9566d0749c4d1b1256cd8c9434f404b421d9185b
Aakash kumar <76447347+itsAakashz@users.noreply.github.com>
2023-07-29 12:58:45 +0530

    Update mail.php

--- Contact/mail.php
+++ Contact/mail.php
@@ -6,7 +6,7 @@ $message= $_POST['message'];
 $to = "gzatrop@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com";
+$headers = "From: noreply@itsaakashz.live";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
